4aaca9f7f1082223f1938361046d62d1f76db84a
Yair Shachar <yairshachar@gmail.com>
2021-08-21 18:30:57 +0300

    added printFieldNamesAndValues() to RunConfigurations

--- src/intray/launcher/RunConfigurations.java
+++ src/intray/launcher/RunConfigurations.java
@@ -1,5 +1,18 @@
 package intray.launcher;
 
+import java.lang.reflect.Field;
+
 public class RunConfigurations {
-	public boolean simulateFirstRun = false;
+	public boolean forceFirstRun = true;
+
+	public void printFieldNamesAndValues() throws IllegalArgumentException, IllegalAccessException {
+		System.out.println("RunConfigurations: ");
+		for (Field field : RunConfigurations.this.getClass().getDeclaredFields()) {
+			field.setAccessible(true);
+			String name = field.getName();
+			Object value = field.get(RunConfigurations.this);
+			System.out.printf("\t%s: %s%n", name, value);
+		}
+		System.out.println("\n\n");
+	}
 }
